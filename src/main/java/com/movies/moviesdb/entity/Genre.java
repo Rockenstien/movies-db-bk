182c89703a4fd8777ec5973f8d3343405f9dcce4
cj-paras <paras@codejudge.io>
2023-05-27 00:43:20 +0530

    Update

--- src/main/java/com/movies/moviesdb/entity/Genre.java
+++ src/main/java/com/movies/moviesdb/entity/Genre.java
@@ -1,11 +1,16 @@
 package com.movies.moviesdb.entity;
 
+import java.util.List;
+
+import com.movies.moviesdb.entity.Movie;
+
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,5 +32,8 @@ public class Genre {
     @Nonnull
     @Column(name = "name")
     @Getter @Setter private String name;
+
+    @ManyToMany
+    private List<Movie> movies;
 
 }
--- src/main/java/com/movies/moviesdb/entity/Movie.java
+++ src/main/java/com/movies/moviesdb/entity/Movie.java
@@ -1,11 +1,18 @@
 package com.movies.moviesdb.entity;
 
+import java.util.List;
+
+import com.movies.moviesdb.entity.Genre;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,4 +47,12 @@ public class Movie {
     private String plot;
 
     private String posterUrl;
+
+    @ManyToOne
+    @JoinTable(
+        name = "movie_genre",
+        joinColumns = @JoinColumn(name = "movie_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name="genre_id", referencedColumnName = "id")
+    )
+    private List<Genre> genres;
 }
